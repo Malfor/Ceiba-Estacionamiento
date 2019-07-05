6985920b8f3766d37bca5685f7d125343526885c
Rodolfo Barrios <rodolfo.9408@gmail.com>
2019-07-05 10:20:03 -0500

    Proyecto back-base

---
+++ src/main/java/com/ceiba/parqueadero/ParqueaderoCeibaApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.parqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoCeibaApplication.class, args);
+	}
+
+}
